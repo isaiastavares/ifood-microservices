62c6053d93d1f458666b550fe702f16f28984db7
isaias.neto <isaias.neto@pcinformatica.com.br>
2018-12-02 20:38:31 -0200

    Padronizando response de error

--- ifood-suggestion-server/src/main/java/br/com/ifood/exception/CityNotFoundException.java
+++ ifood-suggestion-server/src/main/java/br/com/ifood/exception/CityNotFoundException.java
@@ -1,5 +1,9 @@
 package br.com.ifood.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CityNotFoundException extends Exception {
 
 	private static final long serialVersionUID = 7195138072520842587L;
---
+++ ifood-suggestion-server/src/main/java/br/com/ifood/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,71 @@
+package br.com.ifood.exception;
+
+import java.util.Date;
+
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+				request.getDescription(false));
+
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(CityNotFoundException.class)
+	public final ResponseEntity<ExceptionResponse> handleCityNotFoundException(CityNotFoundException ex,
+			WebRequest request) {
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+				request.getDescription(false));
+
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(ConstraintViolationException.class)
+	public final ResponseEntity<ExceptionResponse> handleConstraintViolationException(ConstraintViolationException ex,
+			WebRequest request) {
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+				request.getDescription(false));
+
+		return ResponseEntity.badRequest().body(exceptionResponse);
+	}
+
+	@ExceptionHandler(value = MethodArgumentTypeMismatchException.class)
+	public final ResponseEntity<ExceptionResponse> handleMethodArgumentTypeMismatchException(
+			MethodArgumentTypeMismatchException ex, WebRequest request) {
+		String type = ex.getRequiredType().getSimpleName();
+		String param = ex.getParameter().getParameterName();
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),
+				"Wrong type for parameter: " + param + ". Expected type: " + type, 
+				request.getDescription(false));
+
+		return ResponseEntity.badRequest().body(exceptionResponse);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+				request.getDescription(false));
+
+		return super.handleExceptionInternal(ex, exceptionResponse, headers, status, request);
+	}
+
+}
